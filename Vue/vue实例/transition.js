4434fc9fa191fa9eebdea28dc6ea206588380481
shushengwang <wangshusheng@live.cn>
2018-04-20 18:33:30 +0800

    transition

---
+++ Vue/vue实例/transition.js
@@ -0,0 +1,18 @@
+new Vue({
+    el: '#demo',
+    data: {
+        show: true
+    }
+});
+new Vue({
+    el: '#example-1',
+    data: {
+        show: true
+    }
+});
+new Vue({
+    el: '#example-2',
+    data: {
+        show: true
+    }
+});
